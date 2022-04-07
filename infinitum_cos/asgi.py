087aab3c71441ce42a3da5baa7d8aec0eb3f907a
Mamadaliev Dilshodjon <88366209+DilshodbekMX@users.noreply.github.com>
2022-04-07 10:20:28 +0500

    Add files via upload

---
+++ infinitum_cos/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for infinitum_cos project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'infinitum_cos.settings')
+
+application = get_asgi_application()
---
+++ infinitum_cos/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for infinitum_cos project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'infinitum_cos.settings')
+
+application = get_wsgi_application()
