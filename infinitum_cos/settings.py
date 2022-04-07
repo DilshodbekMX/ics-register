715e478d120275f669b2912a6a437b813fd4c5af
Mamadaliev Dilshodjon <88366209+DilshodbekMX@users.noreply.github.com>
2022-04-07 10:23:02 +0500

    Update settings.py

--- infinitum_cos/settings.py
+++ infinitum_cos/settings.py
@@ -10,7 +10,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%m-d-f9@f)u-oeyy5g)+dp82ioors62fl%f*5xjx$q&qca)zpi'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
